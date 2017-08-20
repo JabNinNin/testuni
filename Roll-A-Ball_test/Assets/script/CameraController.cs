07116967657ca6b8d87cf3c58921eb9cac9e7618
JabNinNin <Mahatthanan57@email.nu.ac.th>
2017-08-20 14:26:25 +0700

    cc

---
+++ Roll-A-Ball_test/Assets/script/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;//ทำให้กล้องหมุนตามมุมมองผู้เล่น
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ Roll-A-Ball_test/Assets/script/playcontrol.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playcontrol : MonoBehaviour {
+    private Rigidbody rb;//ตัวแปรที่ใช้ในการเคลื่อนที่
+    public float speed;
+    // Use this for initialization
+	void Start() {
+        rb = GetComponent<Rigidbody>();
+
+	}
+
+    // Update is called once per frame
+//void Update() {
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 moveMent = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(moveMent*speed);
+        
+	}
+
+    void OntriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Boxa"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+}
